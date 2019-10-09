4d8498cf71264e703c14e7aed3b3ea2d06ddc0b7
Josephine Dorado <josephine@funksoup.com>
2019-10-09 19:05:07 -0400

    adding 06_times

--- questions/06_times/times.js
+++ questions/06_times/times.js
@@ -1,3 +1,11 @@
-function times() {}
+function times(num) {}
+
+	// const startingVal = (num) { x => num * x
+	// }
+
+	const result = (num) { x => num * x
+	}
+ 
+ 	return function(result);
 
 module.exports = { times };
